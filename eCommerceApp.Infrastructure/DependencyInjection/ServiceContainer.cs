03193fcdef56c202d9d3829369125f5af42b72df
abdulrahman <abdulrahmn398@outlook.com>
2025-03-12 16:53:35 +0200

    Add infrastructure services and configuration updates
    - Updated `eCommerceApp.Infrastructure.csproj` to include new package references for Entity Framework Core and configuration abstractions.
    - Modified `appsettings.json` to add a `ConnectionStrings` section for the `eCommerceAppDb` database.
    - Enhanced `eCommerceApp.Host.csproj` with a new package reference for Entity Framework Core Tools.
    - Introduced `ServiceContainer` class in `ServiceContainer.cs` to configure the database context and register generic repositories for `Product` and `Category`.

---
+++ eCommerceApp.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,47 @@
+﻿using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Interfaces;
+using eCommerceApp.Infrastructure.Data;
+using eCommerceApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerceApp.Infrastructure.DependencyInjection;
+
+/// <summary>
+/// Provides extension methods for setting up infrastructure services.
+/// </summary>
+public static class ServiceContainer
+{
+
+    /// <summary>
+    /// Adds infrastructure services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to add services to.</param>
+    /// <param name="configuration">The configuration to use for setting up services.</param>
+    /// <returns>The service collection with the added services.</returns>
+    public static IServiceCollection AddInfrastructureService
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        // Configure the database context
+        var connectionString = "Default";
+        services.AddDbContext<AppDbContext>(option =>
+        option.UseSqlServer(configuration.GetConnectionString(connectionString),
+        sqlOptions =>
+        {
+            // Ensure this is the correct assembly
+            sqlOptions.MigrationsAssembly
+            (typeof(AppContext).Assembly.FullName);
+
+            // Enable automatic retries for transient failure
+            sqlOptions.EnableRetryOnFailure();
+        }),
+        ServiceLifetime.Scoped);
+
+        // Register generic repositories
+        services.AddScoped<IGeneric<Product>, GenericRepository<Product>>();
+        services.AddScoped<IGeneric<Category>, GenericRepository<Category>>();
+
+        return services;
+    }
+}
