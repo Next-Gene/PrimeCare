85716f8c1e4008b5212c13b8a91e28484a73f51a
abdulrahman <abdulrahmn398@outlook.com>
2025-03-13 17:15:41 +0200

    Add CategoryService and ProductService classes
    Introduced `CategoryService` and `ProductService` to handle operations for `Category` and `Product` entities, respectively. Both classes implement their interfaces and utilize a generic interface for CRUD operations. All methods currently throw `NotImplementedException`. Fixed using directives for necessary DTOs. Note: `ProductService` has a typo in the region name ("Mehtods").

---
+++ eCommerceApp.Application/Services/Implementations/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using eCommerceApp.Application.DTOs;
+using eCommerceApp.Application.DTOs.Product;
+using eCommerceApp.Application.Services.Interfaces;
+using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Interfaces;
+
+namespace eCommerceApp.Application.Services.Implementations;
+
+public class ProductService : IProductService
+{
+    private readonly IGeneric<Product> _productInterface;
+
+    public ProductService(IGeneric<Product> productInterface)
+    {
+        _productInterface = productInterface;
+    }
+
+    #region Mehtods
+    public Task<ServiceResponse> AddAsync(CreateProductDto product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<GetProductDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetProductDto> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ServiceResponse> UpdateAsync(UpdateProductDto product)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
