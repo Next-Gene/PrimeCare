91cb91c8ed079442db361f423981b10cd973fb5c
abdulrahman <abdulrahmn398@outlook.com>
2025-03-13 17:31:55 +0200

    Add AutoMapper integration and service registration
    Updated project file to include AutoMapper package.
    Created ServiceContainer for dependency injection and
    added MappingConfiguration for DTO to entity mappings.

---
+++ eCommerceApp.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,30 @@
+﻿using eCommerceApp.Application.Mapping;
+using eCommerceApp.Application.Services.Implementations;
+using eCommerceApp.Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerceApp.Application.DependencyInjection;
+
+/// <summary>
+/// Provides extension methods to register application services in the dependency injection container.
+/// </summary>
+public static class ServiceContainer
+{
+    /// <summary>
+    /// Adds application services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <returns>The <see cref="IServiceCollection"/> with the added services.</returns>
+    public static IServiceCollection AddApplicationService
+        (this IServiceCollection services)
+    {
+        // Configure the AutoMapper
+        services.AddAutoMapper(typeof(MappingConfiguration));
+
+        // Register Services
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        return services;
+    }
+}
---
+++ eCommerceApp.Application/Mapping/MappingConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using eCommerceApp.Application.DTOs.Category;
+using eCommerceApp.Application.DTOs.Product;
+using eCommerceApp.Domain.Entities;
+
+namespace eCommerceApp.Application.Mapping;
+
+/// <summary>
+/// Configuration class for setting up AutoMapper mappings.
+/// </summary>
+public class MappingConfiguration : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MappingConfiguration"/> class.
+    /// Sets up the mappings between DTOs and domain entities.
+    /// </summary>
+    public MappingConfiguration()
+    {
+        CreateMap<CreateCategoryDto, Category>();
+        CreateMap<CreateProductDto, Product>();
+
+        CreateMap<Category, GetCategoryDto>();
+        CreateMap<Product, GetProductDto>();
+    }
+}
