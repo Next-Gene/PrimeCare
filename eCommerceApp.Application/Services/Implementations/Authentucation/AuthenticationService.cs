ab5539aa5b250ebc1bc37589a7dced3fdae0773f
abdulrahman <abdulrahmn398@outlook.com>
2025-03-23 16:16:06 +0200

    Add validation service and improve user creation logic
    Updated `AuthenticationService.cs` to include a new validation service for user creation. Modified the `CreateUser` method to utilize this service for input validation. Added detailed XML documentation for better clarity and enhanced error handling to ensure proper logging and user removal in case of role assignment failures.

--- eCommerceApp.Application/Services/Implementations/Authentucation/AuthenticationService.cs
+++ eCommerceApp.Application/Services/Implementations/Authentucation/AuthenticationService.cs
@@ -3,11 +3,16 @@ using eCommerceApp.Application.DTOs;
 using eCommerceApp.Application.DTOs.Identity;
 using eCommerceApp.Application.Services.Interfaces.Authentucation;
 using eCommerceApp.Application.Services.Interfaces.Logging;
+using eCommerceApp.Application.Validations;
+using eCommerceApp.Domain.Entities.Identity;
 using eCommerceApp.Domain.Interfaces.Authentication;
 using FluentValidation;
 
 namespace eCommerceApp.Application.Services.Implementations.Authentucation;
 
+/// <summary>
+/// Provides authentication services for the application.
+/// </summary>
 public class AuthenticationService : IAuthenticationService
 {
     private readonly ITokenManagement _tokenManagement;
@@ -17,12 +22,25 @@ public class AuthenticationService : IAuthenticationService
     private readonly IMapper _mapper;
     private readonly IValidator<CreateUserDto> _createUserValidator;
     private readonly IValidator<LoginUserDto> _loginUserValidator;
+    private readonly IValidationService _validationService;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthenticationService"/> class.
+    /// </summary>
+    /// <param name="tokenManagement">The token management service.</param>
+    /// <param name="userManagement">The user management service.</param>
+    /// <param name="roleManagement">The role management service.</param>
+    /// <param name="logger">The logger service.</param>
+    /// <param name="mapper">The mapper service.</param>
+    /// <param name="createUserValidator">The validator for creating users.</param>
+    /// <param name="loginUserValidator">The validator for logging in users.</param>
+    /// <param name="validationService">The validation service.</param>
     public AuthenticationService(ITokenManagement tokenManagement,
         IUserManagement userManagement, IRoleManagement roleManagement,
         IAppLogger<AuthenticationService> logger, IMapper mapper,
         IValidator<CreateUserDto> createUserValidator,
-        IValidator<LoginUserDto> loginUserValidator)
+        IValidator<LoginUserDto> loginUserValidator,
+        IValidationService validationService)
     {
         _tokenManagement = tokenManagement;
         _userManagement = userManagement;
@@ -31,17 +49,60 @@ public class AuthenticationService : IAuthenticationService
         _mapper = mapper;
         _createUserValidator = createUserValidator;
         _loginUserValidator = loginUserValidator;
+        _validationService = validationService;
     }
 
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="user">The user to create.</param>
+    /// <returns>A task that represents the asynchronous operation.
+    /// The task result contains a <see cref="ServiceResponse"/> indicating the result of the operation.</returns>
     public async Task<ServiceResponse> CreateUser(CreateUserDto user)
     {
-        var _validation = await _createUserValidator.ValidateAsync(user);
+        var _validationResult = await _validationService
+            .ValidateAsync(user, _createUserValidator);
+        if (!_validationResult.Success) return _validationResult;
 
-        if (!_validation.IsValid)
+        var mappedModel = _mapper.Map<AppUser>(user);
+        mappedModel.UserName = user.Email;
+        mappedModel.PasswordHash = user.Password;
+
+        var result = await _userManagement.CreateUser(mappedModel);
+        if (!result)
+            return new ServiceResponse
+            {
+                Message = "Email Adderss might be already in use or unknown error occurred"
+            };
+
+        var _user = await _userManagement.GetUserByEmail(user.Email);
+        var users = await _userManagement.GetAllUsers();
+        bool assignedResult = await _roleManagement
+            .AddUserToRole(_user!, users!.Count() > 1 ? "User" : "Admin");
+        if (!assignedResult)
         {
-            var errors = _validation.Errors
-                .Select(e => e.ErrorMessage).ToList();
+            //remove user
+            int removeUserResult = await _userManagement
+                .RemoveUserByEmail(_user!.Email!);
+            if (removeUserResult <= 0)
+            {
+                // error occurred roling back change
+                // then log th error
+                _logger.LogError(new Exception(
+                    $"User with Email as {_user.Email} " +
+                    $"faild to remove as result of role assigning issue"),
+                    "User could not be assigend Role");
+                return new ServiceResponse
+                {
+                    Message = "Error occurred in create account"
+                };
+            }
         }
+        return new ServiceResponse
+        {
+            Success = true,
+            Message = "Account Created!"
+        };
     }
 
     public Task<LoginResponse> LoginUser(LoginUserDto user)
